e45f106fbd0e06c7bd2abd35afd74e0ac4f9b942
zero <zeromr@qq.com>
2017-11-03 23:54:27 +0800

    First commit.

---
+++ app/src/main/java/com/demo/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,71 @@
+package com.demo.broadcastbestpractice;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Zero on 2017/11/3.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    private ForceOfflineReceiver receiver;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+            receiver=null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    private class ForceOfflineReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline.Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // 销毁所有活动
+                    ActivityCollector.finishAll();
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    // 重新启动LoginActivity
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
+        }
+    }
+}
---
+++ app/src/main/java/com/demo/broadcastbestpractice/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.demo.broadcastbestpractice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+
+public class LoginActivity extends BaseActivity {
+
+    private EditText et_account;
+    private EditText et_password;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        et_account = ((EditText) findViewById(R.id.et_account));
+        et_password = ((EditText) findViewById(R.id.et_password));
+        btn_login = ((Button) findViewById(R.id.btn_login));
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String account = et_account.getText().toString();
+                String password = et_password.getText().toString();
+                // 如果账号是admin且密码是123456，就认为登录成功
+                if (account.equals("admin") && password.equals("123456")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this,"account or password is invalid",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
